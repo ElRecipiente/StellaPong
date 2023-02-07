9edfa45f112a1f8ea5b98f2f2e6d4360e662d38a
ElRecipiente <nicolas.droux.2b@gmail.com>
2023-02-07 17:09:16 +0100

    fix heigh and add let heightGameWindow

--- script.js
+++ script.js
@@ -2,26 +2,27 @@ let playerOne = document.querySelector("div.racket#P1")
 let playerTwo = document.querySelector("div.racket#P2")
 let pingY = 0;
 let pongY = 0;
+let heightGameWindow = 85;
 
 
 // *** Key Event Listener *** //
 document.addEventListener("keydown", (event) => {
     let key = event.code
     console.log(key)
-    if (key === "ArrowDown" && pongY < 90) {
-        pongY += 2;
+    if (key === "ArrowDown" && pongY < heightGameWindow) {
+        pongY += 5;
         playerTwo.style.top = `${pongY}vh`;
     }
     else if (key === "ArrowUp" && pongY > 0) {
-        pongY -= 2;
+        pongY -= 5;
         playerTwo.style.top = `${pongY}vh`;
     }
-    if (key === "KeyS" && pingY < 90) {
-        pingY += 2;
+    if (key === "KeyS" && pingY < heightGameWindow) {
+        pingY += 5;
         playerOne.style.top = `${pingY}vh`;
     }
     else if (key === "KeyW" && pingY > 0) {
-        pingY -= 2;
+        pingY -= 5;
         playerOne.style.top = `${pingY}vh`;
     }
 })
